7d75873fb24766dec2a2a0850b9287bd2eb6f12e
Рустам <rusiq@list.ru>
2019-11-07 18:25:32 +1000

    мелочь

--- src/config/g_recaptcha.php
+++ src/config/g_recaptcha.php
@@ -5,7 +5,7 @@ return [
     'api_render' => 'https://www.google.com/recaptcha/api.js?render=',
     'api_verify' => 'https://www.google.com/recaptcha/api/siteverify',
     
-    'site_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'site_key' => env('RECAPTCHA_SITE_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+    'secret_key' => env('RECAPTCHA_SECRET_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
     'score' => 0.5,
 ];
