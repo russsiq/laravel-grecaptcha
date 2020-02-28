580108d1bce92f6dc1b04c60ea0c2cdbcdd56782
Рустам <rusiq@list.ru>
2020-02-29 09:39:57 +1000

    Update GRecaptchaContract.php

--- src/app/Contracts/GRecaptchaContract.php
+++ src/app/Contracts/GRecaptchaContract.php
@@ -2,9 +2,17 @@
 
 namespace Russsiq\GRecaptcha\Contracts;
 
+use Illuminate\Contracts\Validation\Validator as ValidatorContract;
+
 interface GRecaptchaContract
 {
     public function input(string $tpl);
     public function script(string $tpl);
+    public function validate(
+        string $attribute,
+        string $value = null,
+        array $parameters = [],
+        ValidatorContract $validator
+    );
     public function verifying();
 }
