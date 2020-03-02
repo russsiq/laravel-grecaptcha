3b18ff7363aa5f43e4bc6efe0b5572fae6fc7820
Рустам <rusiq@list.ru>
2020-03-03 05:55:56 +1000

    Комментирование кода Контракт публичных методов

--- src/app/Contracts/GRecaptchaContract.php
+++ src/app/Contracts/GRecaptchaContract.php
@@ -2,17 +2,49 @@
 
 namespace Russsiq\GRecaptcha\Contracts;
 
+// Сторонние зависимости.
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Contracts\Validation\Validator as ValidatorContract;
 
+/**
+ * Контракт публичных методов валидатора капчи.
+ * @var interface
+ */
 interface GRecaptchaContract
 {
-    public function input(string $tpl);
-    public function script(string $tpl);
+    /**
+     * Получить HTML строковое представление поля ввода капчи пользователем.
+     * @return Htmlable|null
+     */
+    public function input(): ?Htmlable;
+
+    /**
+     * Получить HTML строковое представление JavaScript капчи.
+     * @param  string  $view
+     * @return Htmlable|null
+     */
+    public function script(string $view): ?Htmlable;
+
+    /**
+     * Выполнить валидацию капчи.
+     * @param  string  $attribute
+     * @param  string|null  $userToken
+     * @param  array  $parameters
+     * @param  ValidatorContract  $validator
+     * @return bool
+     */
     public function validate(
         string $attribute,
         string $userToken = null,
         array $parameters = [],
         ValidatorContract $validator
-    );
-    public function verifying(string $secretKey, string $userToken);
+    ): bool;
+
+    /**
+     * Выполнить верификацию токена, полученого из формы от пользователя.
+     * @param  string  $secretKey
+     * @param  string  $userToken
+     * @return bool
+     */
+    public function verifying(string $secretKey, string $userToken): bool;
 }
